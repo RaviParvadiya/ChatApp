8d241fb15d1dee3ce5c9b2952877fe462c1cfd9e
RaviParvadiya <raviparvadiya.2001@gmail.com>
2023-02-01 10:14:11 +0530

    Update JoinRoom

--- src/ChatRoom.jsx
+++ src/ChatRoom.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import './chatRoom.css'
+
+const { io } = require("socket.io-client");
+const socket = io("http://192.168.29.212:3000/");
 
 const ChatRoom = () => {
+  const [msgs, setMsgs] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    socket.on("", (msg) => {
+      setMsgs((msgs) => [...msgs, msg]);
+    });
+
+    socket.on("user-connected", (user) => {
+      setUsers((users) => [...users, user]);
+    });
+
+    socket.on("user-disconnected", (user) => {
+      setUsers((users) => users.filter((u) => u !== user));
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    socket.emit("", input);
+    setInput("");
+  };
+
   return (
-    <div>ChatRoom</div>
-  )
-}
+    <div className="chat-room">
+      <div className="header">
+        <h2>Chat Room</h2>
+        <h3>Connected Users</h3>
+        <ul>
+          {users.map(user => (
+            <li key={user}>{user}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="messages">
+        <ul>
+          {msgs.map(msg => (
+            <li key={msg}>{msg}</li>
+          ))}
+        </ul>
+      </div>
+      <form className="input-form" onSubmit={handleSubmit}>
+        <input value={input} onChange={e => setInput(e.target.value)} />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
+};
 
 export default ChatRoom;
--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import jwt_decode from "jwt-decode";
+import { useNavigate } from "react-router-dom";
 
 const { io } = require("socket.io-client");
 const socket = io("http://192.168.29.212:3000/");
@@ -9,6 +10,8 @@ const Home = () => {
   const [rooms, setRooms] = useState([]);
   const [selectedRoom, setSelectedRoom] = useState("");
 
+  const navigate = useNavigate();
+
   socket.on("connect", () => {
     console.log(socket.id);
   });
@@ -16,7 +19,7 @@ const Home = () => {
   useEffect(() => {
     // listen for room updates from the server
     socket.on("allRooms", (data) => {
-      console.log(data);
+      // console.log(data);
       setRooms(data);
     });
     return () => {
@@ -24,77 +27,67 @@ const Home = () => {
     };
   }, []);
 
-  const handleSubmit = (e) => {
+  const joinRoom = (e) => {
     e.preventDefault();
-    socket.emit("", { roomName: selectedRoom });
+    socket.emit("joinRoom", decoded.username, selectedRoom);
+    navigate("chat-room");
   };
 
   const token = window.localStorage.getItem("token");
-
   const decoded = jwt_decode(token);
 
   const createRoom = async (e) => {
     e.preventDefault();
     // Emit an event to the server to create the room
     socket.emit("joinRoom", decoded.username, room);
-    // navigate("chat-room");
     setRoom("");
   };
 
   return (
     <div className="main-home">
       <div className="home-head">
-      <p className="">Create Room</p>
+        <p className="">Create Room</p>
       </div>
-      
       <div className="create-room-div">
-      <form onSubmit={createRoom}>
-        <div className="room-name-txt">
-        <label>
-          Room Name:
-          <input
-            type="text"
-            value={room}
-            required
-            onChange={(e) => setRoom(e.target.value)}
-          />
-        </label>
-        </div>
-        
-        
-        <button type="submit">Create Room</button>
-      </form>
-      </div>
-
-      <div className="join-room-box">
-
-      <div className="home-head">
-      <p className="">Join Room</p>
-      </div>
-
-      <div className="join-room-div">
-        <form onSubmit={handleSubmit}>
-
-          <div className="select-room-drp-dwn">
-          <label>
-            Select a Room:
-            <select
-              value={selectedRoom}
-              onChange={(e) => setSelectedRoom(e.target.value)}
-            >
-              {rooms.map((room) => (
-                <option key={room.id} value={room.roomName}>
-                  {room.roomName}
-                </option>
-              ))}
-            </select>
-          </label>
+        <form onSubmit={createRoom}>
+          <div className="room-name-txt">
+            <label>
+              Room Name:
+              <input
+                type="text"
+                value={room}
+                required
+                onChange={(e) => setRoom(e.target.value)}
+              />
+            </label>
           </div>
-          
-          <button type="submit">Join Room</button>
+          <button type="submit">Create Room</button>
         </form>
       </div>
-      
+      <div className="join-room-box">
+        <div className="home-head">
+          <p className="">Join Room</p>
+        </div>
+        <div className="join-room-div">
+          <form onSubmit={joinRoom}>
+            <div className="select-room-drp-dwn">
+              <label>
+                Select a Room:
+                <select
+                  value={selectedRoom}
+                  onChange={(e) => setSelectedRoom(e.target.value)}
+                >
+                  {rooms.map((room) => (
+                    <option key={room._id} value={room.roomName}>
+                      {room.roomName}
+                    </option>
+                  ))}
+                </select>
+              </label>
+            </div>
+            <button type="submit">Join Room</button>
+          </form>
+        </div>
       </div>
     </div>
   );
