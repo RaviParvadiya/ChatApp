4e91ea053ce096b3a3b9f00802f7c9ec781f7670
RaviParvadiya <raviparvadiya.2001@gmail.com>
2023-02-06 11:19:32 +0530

    Update

--- src/ChatRoom.jsx
+++ src/ChatRoom.jsx
@@ -10,57 +10,57 @@ const ChatRoom = () => {
   const room = window.localStorage.getItem("room");
   const username = window.localStorage.getItem("username");
 
-  useEffect(()=>{
+  useEffect(() => {
+    socket.emit("joinRoom", username, room);
 
-  socket.emit("joinRoom", username, room);
+    socket.on("message", (msg) => {
+      const messageContainer = document.getElementById("wlc");
+      messageContainer.innerHTML = msg.username + ": " + msg.text;
+    });
 
-  socket.on("message", (msg) => {
-    const messageContainer = document.getElementById("wlc");
-    messageContainer.innerHTML = msg.username + ": " + msg.text;
+    socket.on("info", (msg) => {
+      const messageContainer = document.getElementById("joinedMessage");
+      messageContainer.innerHTML = msg.username + " " + msg.text;
+    });
+
+    socket.emit("getroominfo", room);
+
+    socket.on("allUser", (data) => {
+      const names = data.map((user) => user.username);
+      // console.log('allUser:', names);
+    });
+
+    socket.on("new message", (data) => {
+      console.log("Received message:", data.message, "from", data.name);
+      setMsgs((prevMsgs) => [...prevMsgs, data]);
+    });
+
+    return () => {
+      socket.off("new message");
+    };
   });
-
-  socket.on("info", (msg) => {
-    const messageContainer = document.getElementById("joinedMessage");
-    messageContainer.innerHTML = msg.username + " " + msg.text;
-  });
-
-  socket.emit("getroominfo", room);
-
-  socket.on("allUser", (data) => {
-    const names = data.map((user) => user.username);
-    // console.log('allUser:', names);
-  });
-
-  socket.on("new message", (data) => {
-    console.log("Received message:", data.message, "from", data.name);
-    setMsgs((prevMsgs) => [...prevMsgs, data]);
-  });
-
-  return () => {
-    socket.off("new message");
-  };
-})
 
   const sendMessage = (e) => {
     e.preventDefault();
-    // socket.emit("new message", {room, input, username});
-    socket.emit("new message", room, input, username);
+    socket.emit("new message", room, input);
+    setInput("");
   };
 
   return (
     <div className="chat-room">
       <div id="wlc"></div>
       <div id="joinedMessage"></div>
+      {msgs.map((m) => (
+        <div key={Date.now() + Math.random()}>
+          <p>{m.message}</p>
+          <p>sent by {m.name}</p>
+          <p>By {m.time}</p>
+        </div>
+      ))}
       <form className="input-form" onSubmit={sendMessage}>
         <input value={input} onChange={(e) => setInput(e.target.value)} />
         <button type="submit">Send</button>
       </form>
-      {msgs.map((m) => (
-        <div>
-          <p>{m.message}</p>
-          <p>sent by {m.name}</p>
-        </div>
-      ))}
     </div>
   );
 };
