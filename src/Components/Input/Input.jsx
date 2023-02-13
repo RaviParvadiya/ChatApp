887d1c4aca92769c70db05d27be36d335d521019
RaviParvadiya <raviparvadiya.2001@gmail.com>
2023-02-13 10:10:58 +0530

    Update LeaveRoom

--- src/Components/Input/Input.jsx
+++ src/Components/Input/Input.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
-import './input.css'
+import React from "react";
+import "./input.css";
 
 const Input = ({ message, setMessage, sendMessage }) => {
   return (
     <div className="">
-    <form className="input-form" onSubmit={sendMessage}>
-      <input
-        rows="4"
-        className=""
-        type="text"
-        placeholder="Type a message..."
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
-          <button type='submit'>Send</button>
-    </form>
-  </div>
-  )
-}
+      <form className="input-form" onSubmit={sendMessage}>
+        <input
+          className=""
+          type="text"
+          placeholder="Type a message..."
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
+};
 
-export default Input
+export default Input;
