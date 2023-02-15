996d40e92b696ba0121ab73a443988f8cba31fe0
Kishan Maniya <kishanmaniya001@gmail.com>
2023-02-15 11:05:55 +0530

    responsive-login-signup

--- src/api/API.jsx
+++ src/api/API.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: `http://192.168.29.20:5000/`,
+  baseURL: `http://192.168.29.18:5000/`,
 });
 
-export const APIENDPOINT = "http://192.168.29.20:5000/";
+export const APIENDPOINT = "http://192.168.29.18:5000/";
