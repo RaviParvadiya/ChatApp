a72391d26babbdd89a211991aecbb413aad954e0
Kishan Maniya <kishanmaniya001@gmail.com>
2023-02-16 09:37:42 +0530

    final-commit

--- src/api/API.jsx
+++ src/api/API.jsx
@@ -1,14 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-<<<<<<< HEAD
-  baseURL: `http://localhost:5000/`,
+  baseURL: `https://chat-server-vo3q.onrender.com/`,
 });
 
-export const APIENDPOINT = "http://localhost:5000/";
-=======
-  baseURL: `http://192.168.29.18:5000/`,
-});
-
-export const APIENDPOINT = "http://192.168.29.18:5000/";
->>>>>>> f8d5966f1cfb79a877a46d8e002759c463f756df
+export const APIENDPOINT = "https://chat-server-vo3q.onrender.com/";
