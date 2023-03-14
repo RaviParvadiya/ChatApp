c28548228f9a60c828f5ab416503bacd907ba797
RaviParvadiya <raviparvadiya.2001@gmail.com>
2023-03-14 12:04:06 +0530

    Update Api

--- src/api/API.jsx
+++ src/api/API.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: `http://localhost:5000/`,
+  baseURL: `https://chat-server-vo3q.onrender.com/`,
 });
 
-export const APIENDPOINT = "http://localhost:5000/";
+export const APIENDPOINT = "https://chat-server-vo3q.onrender.com/";
 
 
-//==>  https://chat-server-vo3q.onrender.com
+//==>  http://localhost:5000
 //==>  https://chat-server-vo3q.onrender.com
